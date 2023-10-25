d2acf2e35ac5646de6b12a34a826d16b79d01e75
Erika Contreras <erikacp_1092@hotmail.com>
2023-10-25 15:43:58 -0600

    Added files via upload

---
+++ crud-example/misseri-crud/src/test/java/com/whycompany/payroll/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whycompany.payroll.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
